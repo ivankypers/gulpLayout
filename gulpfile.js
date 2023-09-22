89e1e2af6e696643c245a6860819398e42e1e6e5
CyberGnida <ivan.mol.10@mail.ru>
2023-09-22 21:23:47 +0300

    split gulp build to dev and prod parts

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,26 @@ const image = require('gulp-image');
 const uglify = require('gulp-uglify-es').default;
 const babel = require('gulp-babel');
 const notify = require("gulp-notify");
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del')
+const condition = require('gulp-if')
+const environments = require('gulp-environments')
+const development = environments.development;
+const production = environments.production;
 const browserSync = require('browser-sync').create();
+
+const clean = () => {
+    return del(['dist'])
+}
+
+const resources = () => {
+    return src('src/resources/**')
+        .pipe(dest('dist'))
+}
 
 const styles = () => {
    return src('src/styles/**/*.css')
+       .pipe(development(sourcemaps.init()))
        .pipe(concat('main.css'))
        .pipe(autoprefixes({
            cascade: false,
@@ -20,6 +36,7 @@ const styles = () => {
        .pipe(cleanCSS({
            level: 2,
        }))
+       .pipe(development(sourcemaps.write()))
        .pipe(dest('dist'))
        .pipe(browserSync.stream())
 }
@@ -50,6 +67,7 @@ const scripts = () => {
         'src/js/components/**/*.js',
         'src/js/main.js'
     ])
+        .pipe(development(sourcemaps.init()))
         .pipe(babel({
             presets: ['@babel/env']
         }))
@@ -57,6 +75,7 @@ const scripts = () => {
         .pipe(uglify({
             toplevel: true,
         }).on('error', notify.onError))
+        .pipe(development(sourcemaps.write()))
         .pipe(dest('dist'))
         .pipe(browserSync.stream())
 }
@@ -87,9 +106,11 @@ watch('src/**/*.html', htmlMinify)
 watch("src/**/*.css", styles)
 watch('src/assets/img/*.svg', svgSprites)
 watch('src/js/**/*.js', scripts)
+watch('src/resources/**', resources)
 
 
 exports.scripts = scripts
+exports.clean = clean
 
-exports.default = series(styles, htmlMinify, images, svgSprites, scripts, watchFiles)
+exports.default = series(clean, resources, styles, htmlMinify, images, svgSprites, scripts, watchFiles)
 
