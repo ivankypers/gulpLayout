70f9fdae3ef63ed369010be8884e5e48609016a4
CyberGnida <ivan.mol.10@mail.ru>
2023-09-22 16:03:32 +0300

    layout without adaptive. Added: gulp(htmlMin, concat, image, svgSprites,  clean-css, watchFiles)

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,22 @@ const {src, dest, series, watch} = require('gulp')
 const concat = require('gulp-concat');
 const htmlMin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename')
-const inject = require('gulp-inject');
-const replace = require('gulp-replace')
+const autoprefixes = require('gulp-autoprefixer');
+const svgSprite = require('gulp-svg-sprite');
+const image = require('gulp-image');
+const browserSync = require('browser-sync').create();
 
-
-function styles() {
-    return src('src/styles/**/*.css')
-        .pipe(concat('main.css'))
-        .pipe(cleanCSS())
-        .pipe(dest('dist'));
+const styles = () => {
+   return src('src/styles/**/*.css')
+       .pipe(concat('main.css'))
+       .pipe(autoprefixes({
+           cascade: false,
+       }))
+       .pipe(cleanCSS({
+           level: 2,
+       }))
+       .pipe(dest('dist'))
+       .pipe(browserSync.stream())
 }
 
 const htmlMinify = () => {
@@ -20,9 +26,47 @@ const htmlMinify = () => {
         .pipe(htmlMin({
             collapseWhitespace: true,
         }))
-        .pipe(rename({dirname: ''}))
         .pipe(dest('dist'))
+        .pipe(browserSync.stream())
 }
 
-exports.default = series(styles, htmlMinify)
+const svgSprites = () => {
+    return src('src/assets/img/**/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: '../sprite.svg'
+                }
+            }
+        }))
+        .pipe(dest('dist/images'))
+}
+
+const images = () => {
+    return src([
+        './src/assets/img/**.jpg',
+        './src/assets/img/**.png',
+        './src/assets/img/**.jpeg',
+        './src/assets/img/*.svg',
+        './src/assets/img/**/*.jpg',
+        './src/assets/img/**/*.png',
+        './src/assets/img/**/*.jpeg'
+    ])
+        .pipe(image())
+        .pipe(dest('dist/images'))
+};
+
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: `dist`
+        }
+    })
+}
+
+watch('src/**/*.html', htmlMinify)
+watch("src/**/*.css", styles)
+watch('src/assets/img/*.svg', svgSprites)
+
+exports.default = series(styles, htmlMinify, images, svgSprites, watchFiles)
 
---
+++ src/choices/select.js
@@ -0,0 +1,8 @@
+const tourChoices = document.getElementById("tour");
+const memberChoices = document.getElementById("tour-members");
+const choicesTour = new Choices(tourChoices, {
+    searchEnabled: false,
+});
+const choicesMember = new Choices(memberChoices, {
+    searchEnabled: false,
+});
