34bbc11b1663d194aa6a397be0a5a0deb8abebb5
CyberGnida <ivan.mol.10@mail.ru>
2023-09-22 16:29:45 +0300

    gulp tasks for javascript code

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixes = require('gulp-autoprefixer');
 const svgSprite = require('gulp-svg-sprite');
 const image = require('gulp-image');
+const uglify = require('gulp-uglify-es').default;
+const babel = require('gulp-babel');
+const notify = require("gulp-notify");
 const browserSync = require('browser-sync').create();
 
 const styles = () => {
@@ -42,6 +45,22 @@ const svgSprites = () => {
         .pipe(dest('dist/images'))
 }
 
+const scripts = () => {
+    return src([
+        'src/js/components/**/*.js',
+        'src/js/main.js'
+    ])
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('app.js'))
+        .pipe(uglify({
+            toplevel: true,
+        }).on('error', notify.onError))
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream())
+}
+
 const images = () => {
     return src([
         './src/assets/img/**.jpg',
@@ -67,6 +86,10 @@ const watchFiles = () => {
 watch('src/**/*.html', htmlMinify)
 watch("src/**/*.css", styles)
 watch('src/assets/img/*.svg', svgSprites)
+watch('src/js/**/*.js', scripts)
 
-exports.default = series(styles, htmlMinify, images, svgSprites, watchFiles)
+
+exports.scripts = scripts
+
+exports.default = series(styles, htmlMinify, images, svgSprites, scripts, watchFiles)
 
